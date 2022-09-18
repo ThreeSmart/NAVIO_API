6073746dcdc2788778300b9d0947ebc81fc088c5
Rafael Shakhnazarian <rshakhnazari@adobe.com>
2022-09-18 19:46:52 +0400

    message changed

--- src/main/java/am/threesmart/controllers/ResetPassword.java
+++ src/main/java/am/threesmart/controllers/ResetPassword.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/reset")
 public class ResetPassword {
@@ -21,7 +23,7 @@ public class ResetPassword {
     @PostMapping("/password")
     public ResponseEntity<?> resetPassword(@RequestBody final ResetPasswordCredentials resetPasswordCredentials) {
         userService.resetPassword(resetPasswordCredentials);
-        return ResponseEntity.ok("password_reset");
+        return ResponseEntity.ok(Map.of("response", "Now you can login with your new password!"));
     }
 
 }
