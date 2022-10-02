627d8686fcacc183224cd610d0e641bfce48ff18
Rafael Shakhnazarian <rshakhnazari@adobe.com>
2022-10-02 12:09:22 +0400

    changed image name

--- update_backand.sh
+++ update_backand.sh
@@ -4,6 +4,6 @@ sudo git pull origin master
 
 sudo docker stop threesmart-backend
 sudo docker rm threesmart-backend
-sudo docker rmi navio_api_backend
+sudo docker rmi navioapi_backend
 
 sudo docker-compose up -d
